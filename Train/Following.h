8c06c3bf6dfa059e84e5f39b5cb4332c0c322ab9
Neegbeah Reeves <Nehemiahsal@aol.com>
2015-01-13 16:01:11 -0500

    Before Installing Cocoapods

---
+++ Train/Following.h
@@ -0,0 +1,13 @@
+//
+//  Following.h
+//  Train
+//
+//  Created by Neegbeah Reeves on 1/13/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Following : PFQueryTableViewController
+
+@end
---
+++ Train/Following.m
@@ -0,0 +1,13 @@
+//
+//  Following.m
+//  Train
+//
+//  Created by Neegbeah Reeves on 1/13/15.
+//
+//
+
+#import "Following.h"
+
+@implementation Following
+
+@end
