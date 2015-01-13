6c7c8c2ad35347abf6d347f4e942cc1e30a6232a
Neegbeah Reeves <Nehemiahsal@aol.com>
2015-01-13 15:46:02 -0500

    Add PBJVideoPlayer for Playback

---
+++ Train/RecordingViewController.h
@@ -0,0 +1,13 @@
+//
+//  RecordingViewController.h
+//  Train
+//
+//  Created by Neegbeah Reeves on 1/11/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RecordingViewController : UIViewController
+
+@end
---
+++ Train/RecordingViewController.m
@@ -0,0 +1,37 @@
+//
+//  RecordingViewController.m
+//  Train
+//
+//  Created by Neegbeah Reeves on 1/11/15.
+//
+//
+
+#import "RecordingViewController.h"
+
+@interface RecordingViewController ()
+
+@end
+
+@implementation RecordingViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
