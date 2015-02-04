31ae2637ba501c7cda1453953159acd78ba9616c
Neegbeah Reeves <Nehemiahsal@aol.com>
2015-02-04 18:36:56 -0500

    Add icon artwork. Pending Parse Data

---
+++ Train/FollowingCustomCell.h
@@ -0,0 +1,13 @@
+//
+//  FollowingCustomCell.h
+//  Train
+//
+//  Created by Neegbeah Reeves on 1/27/15.
+//
+//
+
+#import <ParseUI/ParseUI.h>
+
+@interface FollowingCustomCell : PFTableViewCell
+
+@end
---
+++ Train/FollowingCustomCell.m
@@ -0,0 +1,21 @@
+//
+//  FollowingCustomCell.m
+//  Train
+//
+//  Created by Neegbeah Reeves on 1/27/15.
+//
+//
+
+#import "FollowingCustomCell.h"
+
+@implementation FollowingCustomCell
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
