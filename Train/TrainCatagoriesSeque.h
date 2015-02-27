e6daf0b0ebdf6266c6255e3836b4fcd8c79ba408
Neegbeah Reeves <Nehemiahsal@aol.com>
2015-02-26 20:14:43 -0500

    Camera Module Built.

---
+++ Train/TrainCatagoriesSeque.h
@@ -0,0 +1,13 @@
+//
+//  TrainCatagoriesSeque.h
+//  Train
+//
+//  Created by Neegbeah Reeves on 2/26/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TrainCatagoriesSeque : UIStoryboardSegue
+
+@end
---
+++ Train/TrainCatagoriesSeque.m
@@ -0,0 +1,13 @@
+//
+//  TrainCatagoriesSeque.m
+//  Train
+//
+//  Created by Neegbeah Reeves on 2/26/15.
+//
+//
+
+#import "TrainCatagoriesSeque.h"
+
+@implementation TrainCatagoriesSeque
+
+@end
